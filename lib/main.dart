c7588e45198c8aa49b50c66dbd6c8ffbea4cc1a6
eggleeggle <eggleservices@gmail.com>
2024-10-14 05:40:06 -0700

    1213

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       },
       debugShowCheckedModeBanner: false,
       theme: primaryTheme,
-      home: const Splash()
+      home: MyApp()
     );
   }
 }
