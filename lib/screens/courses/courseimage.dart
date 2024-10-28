4103b75baa3a3b9d154396c8f4d9e6650f7b16c5
kirankiran12 <zainab03458717@gmail.com>
2024-10-28 12:53:16 +0500

    image

--- lib/screens/courses/courseimage.dart
+++ lib/screens/courses/courseimage.dart
@@ -12,7 +12,7 @@ class Courseimage extends StatelessWidget {
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/imagecourse.jpg'),
-            fit: BoxFit.contain,
+            fit: BoxFit.fill,
           ),
         ),
 
