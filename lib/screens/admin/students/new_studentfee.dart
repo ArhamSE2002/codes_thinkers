0ea162ff647c1043a112455684fcf5fcccb4a8c3
sanaKhubab <sanakhankhan761@gmail.com>
2024-10-29 00:47:25 -0700

    newstudent

--- lib/screens/admin/students/new_studentfee.dart
+++ lib/screens/admin/students/new_studentfee.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:codes_thinkers/screens/admin/attendance/attendancestudent.dart';
 import 'package:codes_thinkers/screens/admin/students/srudent_fee.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -44,36 +45,35 @@ class _NewstudentState extends State<Newstudent1> {
   @override
   Widget build(BuildContext context) {
     return BackgroundScaffold(
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Column(
+      body: Stack(children: [
+        Column(
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(top: 50, left: 22),
-                      child: const Text(
-                        'Add New Student',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Roboto',
-                        ),
+                Container(
+                  margin: const EdgeInsets.only(top: 30),
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 30),
+                    child: const Text(
+                      '  Add New Student',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Roboto',
                       ),
                     ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Card(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
                   ),
+                )
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Card(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Column(
@@ -83,49 +83,48 @@ class _NewstudentState extends State<Newstudent1> {
                             Center(
                               child: GestureDetector(
                                 onTap: _pickImage,
-                                child: CircleAvatar(
-                                  radius: 50,
-                                  backgroundColor: Colors.blue,
-                                  backgroundImage: _profileImage != null
-                                      ? FileImage(_profileImage!)
-                                      : const AssetImage(
-                                              'assets/images/default_person.png')
-                                          as ImageProvider,
-                                  child: _profileImage == null
-                                      ? const Icon(
-                                          Icons.person,
-                                          size: 60,
-                                          color: Colors.white,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(100),
+                                  child: _profileImage != null
+                                      ? Image.file(
+                                          _profileImage!,
+                                          width: 100,
+                                          height: 100,
+                                          fit: BoxFit.cover,
                                         )
-                                      : null,
+                                      : Container(
+                                          width: 100,
+                                          height: 100,
+                                          color: Colors.blue,
+                                          child: const Icon(
+                                            Icons.person,
+                                            size: 60,
+                                            color: Colors.white,
+                                          ),
+                                        ),
                                 ),
                               ),
                             ),
                             Positioned(
-                              right: 100,
+                              right: 0,
                               bottom: 0,
                               child: GestureDetector(
                                 onTap: _pickImage,
-                                child: const CircleAvatar(
-                                  backgroundColor: Color(0xFFFDD51D),
-                                  radius: 15,
-                                  child: Icon(
-                                    Icons.add,
-                                    color: Colors.black,
-                                    size: 27,
+                                child: const Padding(
+                                  padding: EdgeInsets.only(top: 15, right: 93),
+                                  child: CircleAvatar(
+                                    backgroundColor: Color(0xFFFDD51D),
+                                    radius: 15,
+                                    child: Icon(
+                                      Icons.add,
+                                      color: Colors.black,
+                                      size: 27,
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
                           ],
-                        ),
-                        const SizedBox(height: 20),
-                        TextFormField(
-                          controller: _regnoController,
-                          decoration: const InputDecoration(
-                            labelText: 'Reg no',
-                            border: OutlineInputBorder(),
-                          ),
                         ),
                         const SizedBox(height: 20),
                         TextFormField(
@@ -139,43 +138,51 @@ class _NewstudentState extends State<Newstudent1> {
                         TextFormField(
                           controller: _titleController,
                           decoration: const InputDecoration(
+                            labelText: 'regNo',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          controller: _regnoController,
+                          decoration: const InputDecoration(
                             labelText: 'Title',
                             border: OutlineInputBorder(),
                           ),
+                          minLines: 1,
+                          maxLines: 2,
                         ),
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const Attendancestd()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 10,
-                    ),
-                    backgroundColor: const Color(0xFFFDD51D),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: const Text(
-                    'Add',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
-      ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Attendancestd()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                backgroundColor: const Color(0xFFFDD51D),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Add',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ]),
     );
   }
 }
