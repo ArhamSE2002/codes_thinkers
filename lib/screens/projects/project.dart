ba1a03aabd692a640368b6cdad421ad09e761e03
kirankiran12 <zainab03458717@gmail.com>
2024-10-29 13:45:20 +0500

    project

--- lib/screens/projects/project.dart
+++ lib/screens/projects/project.dart
@@ -1,6 +1,7 @@
 
 import 'dart:io';
 import 'package:codes_thinkers/screens/admin/attendance/attendencestaff.dart';
+import 'package:codes_thinkers/screens/projects/newprojects.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as p;
@@ -15,7 +16,7 @@ class newProject extends StatefulWidget {
 
 class _newProjectState extends State<newProject> {
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController _titlenoController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
   final TextEditingController _aboutController = TextEditingController();
 
   File? _profileImage;
@@ -24,7 +25,7 @@ class _newProjectState extends State<newProject> {
 
   @override
   void dispose() {
-    _titlenoController.dispose();
+    _titleController.dispose();
     _aboutController.dispose();
     super.dispose();
   }
@@ -40,8 +41,7 @@ class _newProjectState extends State<newProject> {
       fileName = p.basename(_profileImage!.path);
     }
   }
-
-  @override
+@override
   Widget build(BuildContext context) {
     return BackgroundScaffold(
       body: Stack(children: [
@@ -57,9 +57,9 @@ class _newProjectState extends State<newProject> {
                     size: 20.0,
                   ),
                 ),
-                Container(   margin: const EdgeInsets.only(top: 30),
+                Container( margin: const EdgeInsets.only(top: 30),
                   child: const Text(
-                    'Add New Project',
+                    'Add New Student',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 25,
@@ -72,97 +72,96 @@ class _newProjectState extends State<newProject> {
               ],
             ),
             const SizedBox(height: 20),
-            // Card wrapping the form fields
-            Card(
-              elevation: 4,
-              margin: const EdgeInsets.symmetric(horizontal: 16),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Stack(
+            Expanded(
+              child: SingleChildScrollView(
+                child: Card(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
                       children: [
-                        Center(
-                          child: GestureDetector(
-                            onTap: _pickImage,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(15), // Circle effect
-                              child: _profileImage != null
-                                  ? Image.file(
-                                      _profileImage!,
-                                      width: 100, // Diameter same as CircleAvatar
-                                      height: 100,
-                                      fit: BoxFit.cover,
-                                    )
-                                  : Container(
-                                      width: 100,
-                                      height: 100,
-                                      color: Colors.blue, // Background color when no image
-                                      child: const Icon(
-                                        Icons.person,
-                                        size: 60,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          right: 133,
-                          bottom: 0,
-                          child: GestureDetector(
-                            onTap: _pickImage,
-                            child: const CircleAvatar(
-                              backgroundColor: Color(0xFFFDD51D),
-                              radius: 15,
-                              child: Icon(
-                                Icons.add,
-                                color: Colors.black,
-                                size: 27,
+                        Stack(
+                          children: [
+                            Center(
+                              child: GestureDetector(
+                                onTap: _pickImage,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(1),
+                                  child: _profileImage != null
+                                      ? Image.file(
+                                          _profileImage!,
+                                          width: 100,
+                                          height: 100,
+                                          fit: BoxFit.cover,
+                                        )
+                                      : Container(
+                                          width: 100,
+                                          height: 100,
+                                          color: Colors.blue,
+                                          child: const Icon(
+                                            Icons.person,
+                                            size: 60,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                ),
                               ),
                             ),
+                            Positioned(
+                              right: 0,
+                              bottom: 0,
+                              child: GestureDetector(
+                                onTap: _pickImage,
+                                child: const CircleAvatar(
+                                  backgroundColor: Color(0xFFFDD51D),
+                                  radius: 15,
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.black,
+                                    size: 27,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          controller: _titleController,
+                          decoration: const InputDecoration(
+                            labelText: 'Title',
+                            border: OutlineInputBorder(),
                           ),
+                        ),
+                       
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          controller: _aboutController,
+                          decoration: const InputDecoration(
+                            labelText: 'About',
+                            border: OutlineInputBorder(),
+                          ),
+                          minLines: 3,
+                          maxLines: 10,
                         ),
                       ],
                     ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      controller: _titlenoController,
-                      decoration: const InputDecoration(
-                        labelText: 'Title',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      controller: _aboutController,
-                      decoration: const InputDecoration(
-                        labelText: 'About',
-                        border: OutlineInputBorder(),
-                      ),
-                      minLines: 3,
-                      maxLines: 10,
-                    ),
-                    const SizedBox(height: 20),
-                  ],
+                  ),
                 ),
               ),
             ),
-            const SizedBox(height: 20),
-            // "Add" button placed outside the card
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const Attendancestaff()),
+                      builder: (context) => const Projects()),
                 );
               },
               style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                 backgroundColor: const Color(0xFFFDD51D),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
@@ -172,10 +171,149 @@ class _newProjectState extends State<newProject> {
                 'Add',
                 style: TextStyle(fontSize: 18, color: Colors.white),
               ),
-            )
+            ),
+            const SizedBox(height: 20),
           ],
         ),
       ]),
     );
   }
 }
+//   @override
+//   Widget build(BuildContext context) {
+//     return BackgroundScaffold(
+//       body: Stack(children: [
+//         Column(
+//           children: [
+//             Row(
+//               children: [
+//                 Container(
+//                   margin: const EdgeInsets.only(top: 30),
+//                   child: const Icon(
+//                     Icons.arrow_back,
+//                     color: Colors.white,
+//                     size: 20.0,
+//                   ),
+//                 ),
+//                 Container(   margin: const EdgeInsets.only(top: 30),
+//                   child: const Text(
+//                     'Add New Project',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 25,
+//                       fontStyle: FontStyle.normal,
+//                       fontWeight: FontWeight.bold,
+//                       fontFamily: 'Roboto',
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             const SizedBox(height: 20),
+//             // Card wrapping the form fields
+//             Card(
+//               elevation: 4,
+//               margin: const EdgeInsets.symmetric(horizontal: 16),
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(15),
+//               ),
+//               child: Padding(
+//                 padding: const EdgeInsets.all(16.0),
+//                 child: Column(
+//                   children: [
+//                     Stack(
+//                       children: [
+//                         Center(
+//                           child: GestureDetector(
+//                             onTap: _pickImage,
+//                             child: ClipRRect(
+//                               borderRadius: BorderRadius.circular(15), // Circle effect
+//                               child: _profileImage != null
+//                                   ? Image.file(
+//                                       _profileImage!,
+//                                       width: 100, // Diameter same as CircleAvatar
+//                                       height: 100,
+//                                       fit: BoxFit.cover,
+//                                     )
+//                                   : Container(
+//                                       width: 100,
+//                                       height: 100,
+//                                       color: Colors.blue, // Background color when no image
+//                                       child: const Icon(
+//                                         Icons.person,
+//                                         size: 60,
+//                                         color: Colors.white,
+//                                       ),
+//                                     ),
+//                             ),
+//                           ),
+//                         ),
+//                         Positioned(
+//                           right: 133,
+//                           bottom: 0,
+//                           child: GestureDetector(
+//                             onTap: _pickImage,
+//                             child: const CircleAvatar(
+//                               backgroundColor: Color(0xFFFDD51D),
+//                               radius: 15,
+//                               child: Icon(
+//                                 Icons.add,
+//                                 color: Colors.black,
+//                                 size: 27,
+//                               ),
+//                             ),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                     const SizedBox(height: 20),
+//                     TextFormField(
+//                       controller: _titlenoController,
+//                       decoration: const InputDecoration(
+//                         labelText: 'Title',
+//                         border: OutlineInputBorder(),
+//                       ),
+//                     ),
+//                     const SizedBox(height: 20),
+//                     TextFormField(
+//                       controller: _aboutController,
+//                       decoration: const InputDecoration(
+//                         labelText: 'About',
+//                         border: OutlineInputBorder(),
+//                       ),
+//                       minLines: 3,
+//                       maxLines: 10,
+//                     ),
+//                     const SizedBox(height: 20),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(height: 20),
+//             // "Add" button placed outside the card
+//             ElevatedButton(
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                       builder: (context) => const Attendancestaff()),
+//                 );
+//               },
+//               style: ElevatedButton.styleFrom(
+//                 padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+//                 backgroundColor: const Color(0xFFFDD51D),
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(10),
+//                 ),
+//               ),
+//               child: const Text(
+//                 'Add',
+//                 style: TextStyle(fontSize: 18, color: Colors.white),
+//               ),
+//             )
+//           ],
+//         ),
+//       ]),
+//     );
+//   }
+// }
